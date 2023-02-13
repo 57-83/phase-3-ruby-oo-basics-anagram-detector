d6c355ba08b22d80c636a50787b1147d8ae81f7e
+ <+>
2023-02-13 23:00:45 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w) }
+    end
+  
+    private
+  
+    def anagram?(other_word)
+      return false if other_word.length != @word.length
+      return false if other_word.downcase == @word.downcase
+  
+      other_word.downcase.chars.sort == @word.downcase.chars.sort
+    end
+  end
+  
